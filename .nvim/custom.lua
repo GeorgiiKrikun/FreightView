31da462e969adedccf3bdf40100f3e2c11a6303f
Georgii <georgii.krikun@celantur.com>
2025-05-11 18:20:23 +0200

    Publishing crate

--- .nvim/custom.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_user_command("DdiveDebug", function()
-	vim.cmd(":GdbStart gdb -q --args target/debug/ddive ddive-test-img:latest")
-end, {})
-
-vim.keymap.set("n", "<leader>bb", ":copen | AsyncRun cargo build<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>bt", ":copen | AsyncRun cargo test<CR>", { noremap = true, silent = true })
